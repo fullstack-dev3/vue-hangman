082896adc4687a41dc50f8856c96a983c8b9d0d1
full-stack-david <davidchan12323@gmail.com>
2023-09-02 02:30:01 -0500

    Add multi test progress

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="container">
     <div class="mt-3 reset">
-      <button class="btn btn-primary" v-if="count > 0" @click="onReset">
+      <button class="btn btn-primary" v-if="count > 0 && current != total" @click="onReset">
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
           <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
         </svg>
         Reset
       </button>
+    </div>
+    <div class="progress mt-3">
+      <div
+        class="progress-bar progress-bar-striped progress-bar-animated bg-success"
+        role="progressbar"
+        :aria-valuenow="Math.floor(current / total * 100)"
+        aria-valuemin="0"
+        aria-valuemax="100"
+        :style="'width: ' + Math.floor(current / total * 100) + '%'"
+      >
+        {{ Math.floor(current / total * 100) }}%
+      </div>
     </div>
     <div class="word">
       <div
@@ -47,7 +59,18 @@ export default {
     SimpleKeyboard
   },
   setup: () => {
-    const word = words[Math.floor(Math.random() * (words.length - 1))];
+    let total = 0;
+    const current = 0;
+    const tasks = [];
+    while (tasks.length < 5) {
+      const task = words[Math.floor(Math.random() * (words.length - 1))];
+      if (!tasks.includes(task)) {
+        tasks.push(task);
+        total += task.length;
+      }
+    }
+
+    const word = tasks[0];
 
     const rand1 = Math.floor(Math.random() * (word.length - 2)) + 1;
     let rand2 = -1;
@@ -67,9 +90,12 @@ export default {
       }
     }
 
-    const count = ref(0)
+    const count = ref(0);
 
     return {
+      tasks,
+      total,
+      current,
       word,
       letters,
       rand1,
@@ -88,6 +114,22 @@ export default {
           }
         }
         this.count++;
+        
+        if (this.count == this.letters.length) {
+          let letters = '';
+          for (let i = 0; i < this.letters.length; i++) {
+            letters += this.letters[i];
+          }
+
+          if (this.word == letters) {
+            this.current += this.count;
+
+            if (this.tasks.length > 1) {
+              this.tasks.shift();
+              this.next();
+            }
+          }
+        }
       }
     },
     onReset() {
@@ -101,6 +143,33 @@ export default {
 
       this.count = 0;
     },
+    next() {
+      const word = this.tasks[0];
+
+      const rand1 = Math.floor(Math.random() * (word.length - 2)) + 1;
+      let rand2 = -1;
+      if (word.length > 4) {
+        rand2 = rand1;
+        while (rand1 == rand2) {
+          rand2 = Math.floor(Math.random() * (word.length - 2)) + 1;
+        }
+      }
+
+      let letters = [];
+      for (let i = 0; i < word.length; i++) {
+        if (rand1 == i || rand2 == i) {
+          letters.push(word[i]);
+        } else {
+          letters.push('');
+        }
+      }
+
+      this.word = word;
+      this.letters = letters;
+      this.rand1 = rand1;
+      this.rand2 = rand2;
+      this.count = ref(0);
+    }
   },
   watch: {
     letters(letters) {
@@ -125,6 +194,9 @@ body {
 }
 .reset {
   min-height: 50px;
+}
+.progress {
+  margin: 0 50px;
 }
 .word {
   align-items: center;
