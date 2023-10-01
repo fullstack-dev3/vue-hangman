e9aa8b3c96608a3e628ecec50664e4c4f1c5d4d8
full-stack-david <davidchan12323@gmail.com>
2023-10-01 01:22:49 -0500

    Update homepage with loading screen

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,7 +1,20 @@
 <template>
-  <div class="container">
+  <div class="home">
     <div class="loading" v-if="loading">
-      <img v-bind:src="require('../assets/home.png')" />
+      <div class="logo">
+        <img src="../assets/logo.png" height="136" width="760" />
+      </div>
+      <div class="progress">
+        <div
+          class="progress-bar"
+          role="progressbar"
+          :aria-valuenow="percent"
+          aria-valuemin="0"
+          aria-valuemax="100"
+          :style="'width: ' + percent + '%'"
+        >
+        </div>
+      </div>
     </div>
     <div v-else>
 
@@ -14,21 +27,57 @@ export default {
   name: "HomePage",
   data() {
     return {
-      loading: true
+      loading: true,
+      percent: 0
     }
   },
   created() {
-    setTimeout(() => {
-      this.loading = false;
-    }, 1500);
+    setInterval(() => {
+      if (this.percent < 60) {
+        this.percent = this.percent + 3;
+      } else if (this.percent < 100) {
+        this.percent++
+      } else {
+        setTimeout(() => {
+          this.loading = false;
+        }, 1500);
+      }
+    }, 100);
   }
 };
 </script>
 
 <style lang="less">
+.home {
+  background-image: url('../assets/home.png');
+  background-position: center;
+  height: 100vh;
+}
 .loading {
   display: flex;
+  flex-direction: column;
   height: 100vh;
   justify-content: center;
+}
+.loading .logo {
+  align-items: center;
+  display: flex;
+  flex: 1;
+  justify-content: center;
+}
+.loading .progress {
+  background: transparent;
+  border-radius: 15px;
+  height: 30px;
+  margin: 0 auto;
+  padding-bottom: 30vh;
+  width: 33vw;
+}
+.loading .progress .progress-bar {
+  background-color: white;
+  border-radius: 15px;
+  color: green;
+  font-size: 18px;
+  height: 30px;
 }
 </style>
