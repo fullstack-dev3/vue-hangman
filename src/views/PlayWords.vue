50dac771b0467119f3cadd31c2eedacfef5f34cc
full-stack-david <davidchan12323@gmail.com>
2023-10-05 01:14:06 -0500

    Update play page with actions

--- src/views/PlayWords.vue
+++ src/views/PlayWords.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="container d-flex flex-column justify-content-between py-5">
+    <img class="back" src="../assets/back.png" @click="goBack" />
     <div class="d-flex align-items-center justify-content-center">
       <img :src="getImgUrl(category)" v-bind:alt="category">
       <h1 class="title">{{ category }}</h1>
     </div>
     <div>
       <div class="mt-3 reset">
-        <button class="btn btn-primary" v-if="count > 0 && current != total" @click="onReset">
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
-            <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
-          </svg>
-          Reset
-        </button>
+        <img src="../assets/reset.png" v-if="count > 0 && current != total" @click="onReset" />
       </div>
       <div class="progress mt-3">
         <div
@@ -133,6 +128,9 @@ export default {
     this.count = ref(0);
   },
   methods: {
+    goBack() {
+      this.$router.push('/playnow');
+    },
     getImgUrl(url) {
       const images = require.context('../assets/', false, /\.png$/);
       return images('./' + url + ".png");
@@ -219,6 +217,12 @@ export default {
 .container {
   min-height: 100vh;
 }
+.back {
+  cursor: pointer;
+  height: 60px;
+  position: absolute;
+  width: 60px;
+}
 .title {
   color: white;
   font-family: cursive;
@@ -229,6 +233,11 @@ export default {
 }
 .reset {
   min-height: 50px;
+}
+.reset img {
+  cursor: pointer;
+  height: 50px;
+  width: 50px;
 }
 .progress {
   margin: 0 50px;
